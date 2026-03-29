eedc7eba3c3b250ddaf3af40c98be60002a1b36e
Arashiiiii <arashizou@gmail.com>
2026-03-29 01:43:50 +0100

    update

--- app/auth/sign-up/page.tsx
+++ app/auth/sign-up/page.tsx
@@ -1,5 +1,7 @@
 import { SignUpForm } from "@/components/sign-up-form";
 
+export const dynamic = "force-dynamic";
+
 export default async function Page({
   searchParams,
 }: {
@@ -10,7 +12,7 @@ export default async function Page({
 
   return (
     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
-      <div className="w-full max-w-sm">
+      <div className="w-full max-w-md">
         <SignUpForm role={role} />
       </div>
     </div>
